683456214c1eedcfcbb0019f37bfd0a291750d26
Pepe González <pepeglezabad@gmail.com>
2019-09-20 16:58:10 +0200

    Update send.php

--- send.php
+++ send.php
@@ -6,8 +6,10 @@
 	$msg = $_POST['msg'];
 	$current_time = exec('date +%R');	
 	// Database query
-	$sql = "INSERT INTO posts(username, msg, date) VALUES('$username','$msg', '$current_time');";
-	$query = pg_query($dbconn, $sql);
+	if(!pg_connection_busy($dbconn)) {
+		$sql = "INSERT INTO posts(username, msg, date) VALUES('$username','$msg', '$current_time');";
+		$query = pg_query($dbconn, $sql);
+	}
 	
 	header("Location: home.php");
 ?>
